b520743c33de179d4972a4a8c6e20241a8adeea4
Nizamov Timur <3dieza@gmail.com>
2023-01-11 19:17:29 +0300

    branch-2

--- src/java/main/kata_git/Main.java
+++ src/java/main/kata_git/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hi there");
         System.out.println("hi git");
+        System.out.println("hi branch-2");
     }
 }
